a47cb8d39e4ee94a374ed9a5f7b4225d6bca27fc
Jesco <JescoVogt@web.de>
2026-03-29 19:14:19 +0200

    yeah right

--- hosts/tower/home.nix
+++ hosts/tower/home.nix
@@ -1,19 +1,19 @@
 { illogical-flake, inputs, ... }: {
-  home-manager.users.jesco = {
-    imports = [
-      ../../modules/home/common.nix
-      inputs.zen-browser.homeModules.beta
-    ];
+  imports = [
+    ../../modules/software
+    ../../modules/common.nix
+    ../../modules/home.nix
+    ../../modules/security.nix
+    ../../modules/session.nix
+  ];
 
-    myHome = {
-        packages.enable = true;
-        hyprland = {
-          enable = true;
-          monitors = [
-            "desc:BNQ BenQ GL2450 R4E01733019,preferred,0x0,1"
-            "desc:BNQ BenQ GL2450H S5D02889019,preferred,-1920x0,1"
-          ];
-        };
-      };
+  myHome = {
+    hyprland = {
+      monitors = [
+        "desc:BNQ BenQ GL2450 R4E01733019,preferred,0x0,1"
+        "desc:BNQ BenQ GL2450H S5D02889019,preferred,-1920x0,1"
+      ];
+    };
   };
+  home-manager.backupFileExtension = "backup";
 }
