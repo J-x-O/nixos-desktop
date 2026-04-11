4d6986537ea4954ddd243d8d896ff028da38188b
Jesco <JescoVogt@web.de>
2026-04-12 00:59:05 +0200

    setup dolphin for files

--- modules/common.nix
+++ modules/common.nix
@@ -58,6 +58,7 @@
     wget
     curl
     git
+    fastfetch
   ];
 
   # germansky
--- modules/software/hyprland.nix
+++ modules/software/hyprland.nix
@@ -36,6 +36,7 @@
       xdg.mimeApps = {
         enable = true;
         defaultApplications = {
+          "inode/directory" = "org.kde.dolphin.desktop";
           "text/html" = "zen.desktop";
           "x-scheme-handler/http" = "zen.desktop";
           "x-scheme-handler/https" = "zen.desktop";
